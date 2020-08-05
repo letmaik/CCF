98bf4ba98b7ba78254411bd37c40398f36b63071
Maik Riechert <maik.riechert@microsoft.com>
2020-08-05 10:40:35 +0000

    add simple test website

--- tests/modules/src/endpoints.js
+++ tests/modules/src/endpoints.js
@@ -1,15 +1,12 @@
 import {partition} from 'lodash-es'
 import * as rs  from 'jsrsasign';
-import * as pbjs from './protobuf.js'
-
-console.log(partition)
-console.log(pbjs)
-console.log(rs)
+import * as pbjs from './libs/protobuf.js'
 
 export function put() {
-
+    console.log(partition)
 }
 
 export function get() {
-
+    console.log(pbjs)
+    console.log(rs)
 }
